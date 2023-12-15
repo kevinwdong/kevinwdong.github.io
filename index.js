7dd0daa3688c02796f3479a64f1bb2e326f8141b
mrheinekev@gmail.com <mrheinekev@gmail.com>
2023-12-16 12:46:58 +1300

    adjust navbar height and change bgc

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
-    document.getElementById("homeMain").style.marginRight = "200px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mainBox").style.marginRight = "200px";
+    document.body.style.backgroundColor = " rgb(85, 132, 225)";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("mainBox").style.marginRight= "0";
+    document.body.style.backgroundColor = "#DDF2FD";
   }
